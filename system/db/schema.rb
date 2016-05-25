2d7cdb7de583f52c56834b36daa056f81cc2925a
Grant Regimbal <regimbal.grant@gmail.com>
2016-05-25 14:16:28 -0700

    rework migration

--- system/db/schema.rb
+++ system/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525013316) do
+ActiveRecord::Schema.define(version: 20160525210718) do
 
   create_table "servers", force: :cascade do |t|
     t.string   "title"
